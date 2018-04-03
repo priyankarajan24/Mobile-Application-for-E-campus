63cc0968ff9d4642d2f74dc23b7b4829d2f36598
priyankarajan24 <34530293+priyankarajan24@users.noreply.github.com>
2018-04-03 11:19:25 +0530

    Update Config.java

--- src/com/example/campus/Config.java
+++ src/com/example/campus/Config.java
@@ -1,6 +1,6 @@
 	package com.example.campus;
 
 	public class Config {
-	    public static final String EMAIL ="mailjava05@gmail.com";
-	    public static final String PASSWORD ="testingme";
+	    public static final String EMAIL ="ecampuskgisl@gmail.com";
+	    public static final String PASSWORD ="kgisl";
 	}
